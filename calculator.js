8dca67deb74b8953a7deb048db69543fef079fc6
Haris <amin.haris3323@gmail.com>
2021-09-14 23:45:56 +0700

    Pure DOM Manipulation

--- calculator.js
+++ calculator.js
@@ -1,17 +1,15 @@
-$(".calculation").on('click',function(){
-  if($(this).attr('item')!='operator'){
-    $(".display").val($(".display").val()+$(this).val());
-  }
-  else{
-    if($(".display").val()!=''){
-      $(".display").val($(".display").val()+$(this).val());
-    }
-  }
-});
-$(".clear").on('click',function(){
-  $(".display").val('');
-});
-$(".equals").on('click',function(){
-  var expression = $(".display").val();
-  $(".display").val(eval(expression));
-});
+var calculation = document.querySelectorAll(".calculation")
+var display = document.querySelectorAll(".display")[0]
+var clear = document.querySelectorAll(".clear")[0]
+var equals = document.querySelectorAll(".equals")[0]
+for(i=0; i<calculation.length; i++){
+  calculation[i].addEventListener("click",function (){
+    display.value = display.value+this.value;
+  });
+  clear.addEventListener("click",function (){
+    display.value = "";
+  })
+  equals.addEventListener("click",function (){
+    display.value = eval(display.value)
+  })
+}
